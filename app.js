b0196a0268592176f19352c7f309b47f0ec337d2
cophi <pzarindast@gmail.com>
2022-07-12 11:34:00 +0200

    changed up the layout and content

--- app.js
+++ app.js
@@ -66,7 +66,12 @@ function cursor(e) {
 }
 function activeCursor(e) {
   const item = e.target;
-  if (item.id === "logo" || item.classList.contains("burger")) {
+  if (
+    item.id === "logo" ||
+    item.classList.contains("burger") ||
+    item.id === "contact-burger-text" ||
+    item.id === "outro"
+  ) {
     mouse.classList.add("nav-active");
   } else {
     mouse.classList.remove("nav-active");
@@ -87,7 +92,7 @@ function navToggle(e) {
     gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
     gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
     gsap.to("#logo", 1, { color: "black" });
-    gsap.to("#contact-burger-text", 1, { scale: "0" });
+    gsap.to("#contact-burger-text", 1, { scale: "0", color: "black" });
     gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
     document.body.classList.add("hide");
   } else {
@@ -95,7 +100,7 @@ function navToggle(e) {
     gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
     gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
     gsap.to("#logo", 1, { color: "white" });
-    gsap.to("#contact-burger-text", 1, { scale: "1" });
+    gsap.to("#contact-burger-text", 1, { scale: "1", color: "white" });
     gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
     document.body.classList.remove("hide");
   }

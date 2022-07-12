ab681827e89aa576e46cd82add565a7c19a88b98
cophi <pzarindast@gmail.com>
2022-07-12 23:57:47 +0200

    no zoom out with scroll

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function animateSlides() {
     const pageTl = gsap.timeline();
     let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
     pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
-    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 1 });
     pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
     //Create new scene
     pageScene = new ScrollMagic.Scene({
